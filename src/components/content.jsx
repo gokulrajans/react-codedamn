093a33a0be1ad2f73ab1a137f57a4a79bd03ec77
GOKULRAJ <gokul19.v@gmail.com>
2022-04-23 06:15:39 +0530

    code has been optimised and forms and submit has been used here.

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
 
-
-
 function Content() {
 
     const [fruits, setValue] = useState(["Apple", "Mango", "Pinapple", "Orange"]);
     const [todo, setTodo] = useState("");
 
-    function addContent() {
+    function addContent(event) {
+        event.preventDefault();
+
         setValue(oldVal => {
             return [...oldVal, todo]
-        })
+        });
+        setTodo("");
+
+    }
+
+    function keydownfunction(e) {
+        if (e.keyCode == 13) {
+            addContent();
+        }
     }
     return <><h1>Best Application to print Down...</h1>
-        <input type="text" value={todo} onChange={ev => {
-            setTodo(ev.target.value);
-        }}></input>
-        <button onClick={addContent}>Add the content</button>
+        <form onSubmit={addContent}>
+            <input onKeyDown={keydownfunction} type="text" value={todo} onChange={ev => {
+                setTodo(ev.target.value);
+            }}></input>
+            <button type="Submit">Add the content</button>
+        </form>
         <ul>
             {fruits.map((item) => { return <li>{item}</li> })}
         </ul>
