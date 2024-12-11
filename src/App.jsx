0cb631ba8b71da2154e25c394cbc94e5a57fae01
GOKULRAJ VARATHARAJAN <gokul19.v@gmail.com>
2024-12-11 06:49:19 +0530

    11/12/2024 practices...

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
 import react from "react";
 import Header from "./components/header";
-import Content from "./components/content"
+import Content from "./components/content";
 import Footer from "./components/footer";
+import { Test } from "./components/Test";
 
 function App() {
-    return(<><Content></Content></>)
+  var name = "Gokul";
+  let letName = "Poongodi";
+
+  function testApp() {
+
+    name = "FunName";
+    // letName = "letFunName";
+    console.log(name);
+    console.log(letName);
+  }
+
+  testApp();
+
+
+  let jsonData = {
+    id: 123,
+    name: {
+      name1: "Gokul",
+      name2: {
+        name3: "Rajan"
+      }
+    },
+    city: "Salem"
+  }
+  console.log(JSON.stringify(jsonData), "Stringify");
+  console.log(JSON.parse(JSON.stringify(jsonData)), "parse");
+
+  const case1 = 2
+  switch(case1) {
+    case 1:
+      return "20";
+
+  }
+  return (
+    <>
+      <Test></Test>
+    </>
+  );
 }
 
-export default App
+export default App;
