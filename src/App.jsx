9f438471f3370428f46722b143ae8ea555bf5e44
GOKULRAJ VARATHARAJAN <gokul19.v@gmail.com>
2024-12-15 11:34:33 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,7 @@ import Footer from "./components/footer";
 import { Test } from "./components/Test";
 
 function App() {
-  var name = "Gokul";
-  let letName = "Poongodi";
-
-  function testApp() {
-
-    name = "FunName";
-    // letName = "letFunName";
-    console.log(name);
-    console.log(letName);
-  }
-
-  testApp();
-
-
-  let jsonData = {
-    id: 123,
-    name: {
-      name1: "Gokul",
-      name2: {
-        name3: "Rajan"
-      }
-    },
-    city: "Salem"
-  }
-  console.log(JSON.stringify(jsonData), "Stringify");
-  console.log(JSON.parse(JSON.stringify(jsonData)), "parse");
-
-  const case1 = 2
-  switch(case1) {
-    case 1:
-      return "20";
-
-  }
+ 
   return (
     <>
       <Test></Test>
